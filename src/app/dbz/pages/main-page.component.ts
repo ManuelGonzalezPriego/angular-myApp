7006f1edffca90afa78507dc7ceef7cb68e99535
Manuel <manuelgonzalezpriego1@gmial.com>
2024-11-26 15:38:42 +0100

    26/11/2024

---
+++ src/app/dbz/pages/main-page.component.ts
@@ -0,0 +1,12 @@
+import { Component, OnInit } from "@angular/core";
+
+@Component({
+  selector: 'app-dbz-main-name',
+  templateUrl: './main-page.component.html'
+})
+
+export class MainPageComponent implements OnInit{
+  constructor(){}
+
+  ngOnInit() { }
+}
