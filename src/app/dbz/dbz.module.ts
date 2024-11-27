11cb16b0e126a8aedb32fffff71a12c120158930
Manuel <manuelgonzalezpriego1@gmial.com>
2024-11-27 15:54:35 +0100

    Update 27/11/2024

--- src/app/dbz/dbz.module.ts
+++ src/app/dbz/dbz.module.ts
@@ -1,15 +1,13 @@
 import { NgModule } from "@angular/core";
 import { CommonModule } from "@angular/common";
 import { MainPageComponent } from "./pages/main-page.component";
+import { ListaComponent } from "./components/lista/lista.component";
+import { PersonajeComponent } from "./components/personaje/personaje.component";
 
 
 @NgModule({
-  declarations:[MainPageComponent],
-  imports:[
-    CommonModule
-  ],
-  exports:[
-    MainPageComponent
-  ]
+  declarations:[MainPageComponent,ListaComponent,PersonajeComponent],
+  imports:[CommonModule],
+  exports:[MainPageComponent]
 })
 export class DbzModule {}
